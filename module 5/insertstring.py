850ea29ca2cfacf9bb3a32b5733f305358bc2499
akeith997 <79158451+akeith997@users.noreply.github.com>
2021-02-16 12:10:26 -0500

    Update insertstring.py

--- module 5/insertstring.py
+++ module 5/insertstring.py
@@ -1,3 +1,3 @@
 def insert_string_middle(str, word):
-    return str[:2] + word + str[2:] #Appends to front and back of string
-print(insert_string_middle('<<>>', 'LIS4930')) 
+    return str[:3] + word + str[3:] #Appends to front and back of string
+print(insert_string_middle('<<<>>>', 'LIS4930')) 
