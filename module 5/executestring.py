d49003cd659b7e8ebb2c79e4e8cd14bb79bd2853
akeith997 <79158451+akeith997@users.noreply.github.com>
2021-02-16 11:59:10 -0500

    Update executestring.py

--- module 5/executestring.py
+++ module 5/executestring.py
@@ -1,1 +1,17 @@
 
+def name(num):
+    translation = {
+        "0": "John",
+        "1": "Bob",
+        "2": "Steve",
+        "3": "Kevin",
+        "4": "Austin",
+        "5": "Dylan"
+    }
+    maybe_name = translation.get(num)
+    if maybe_name is None:
+        raise ValueError("{num} is not a valid option.")
+    return maybe_name
+
+inputname = raw_input("Enter a number 0-5.\n")
+print(name(inputname))
