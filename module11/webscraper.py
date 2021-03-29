993ea58f8922b8a140884fb84497c0a3aad6e7cf
akeith997 <79158451+akeith997@users.noreply.github.com>
2021-03-28 20:16:50 -0400

    Create webscraper.py

---
+++ module11/webscraper.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
+
+
+url = 'http://web.mta.info/developers/turnstile.html'
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+line_count = 1 #variable to track what line you are on
+for one_a_tag in soup.findAll('a'):  #'a' tags are for links
+    if line_count >= 36: #code for text files starts at line 36
+        link = one_a_tag['href']
+        download_url = 'http://web.mta.info/developers/'+ link
+        urllib.request.urlretrieve(download_url,'./'+link[link.find('/turnstile_')+1:]) 
+        time.sleep(1) #pause the code for a sec
+    #add 1 for next line
+    line_count +=1
